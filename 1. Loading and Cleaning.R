9ffd3833c7d1976df77f9fc711439e04a23dd854
Kaellas <pawelgach@midea.pl>
2023-11-30 12:50:50 +0100

    Version 0.2

--- 1. Loading and Cleaning.R
+++ 1. Loading and Cleaning.R
@@ -1,13 +1,17 @@
 #
 # ──────────────────────────────────────────────────────────────────────────────
+#
 # SECTION 1: LOADING AND CLEANING
+#
 # ──────────────────────────────────────────────────────────────────────────────
 #
 # Libraries used:
 
 library(skimr)
+library(ggplot2)
 library(readxl)
 library(jsonlite)
+library(stringr)
 
 # SECTION TABLE OF CONTENTS
 #
@@ -35,7 +39,7 @@ library(jsonlite)
 # EXTRACTION
 # ──────────────────────────────────────────────────────────────────────────────
 #
-# As shown in Section 1, extracting US-pumpkins can be done in one line:
+# Extracting US-pumpkins is simple can be done in one line:
 
 pumpkin <- read.csv("US-pumpkins.csv")
 
@@ -59,8 +63,8 @@ data <- jsonlite::fromJSON("filepath.json")
 
 View(pumpkin)
 
-# After that, like in the first Section, I recommend the skimr library
-# It returns the most important informations in one go
+# After that, I recommend the skimr library, as it 
+# returns the most important informations in one go
 
 skimr::skim(pumpkin)
 
@@ -81,8 +85,131 @@ summary(pumpkin)
 str(pumpkin)
 #displays the structure of an object
 
+unique(pumpkin$Repack)
+#display all unique column elements
+
 # ──────────────────────────────────────────────────────────────────────────────
-# MANIPULATION
+# MANIPULATION (INCL. COMMON PROBLEMS)
 # ──────────────────────────────────────────────────────────────────────────────
+
+subset(pumpkin, High.Price > 100)
+# Use to select relevant columns
+
+pumpkin$Repack <- factor(pumpkin$Repack)
+# turn characters into numerical factors that models understand
+
+tapply(pumpkin$High.Price, pumpkin$City.Name, mean)
+# apply functions to grouped columns for quick analysis
+
+colSums(pumpkin[,8:9])
+rowSums(pumpkin[1:100,8:9])
+# Get sums of rows and columns, respecitvely
+
+colnames(pumpkin)[1] <- "city.name"
+#change names of columns
+
+# DELETING COLUMNS
+# There are multiple ways to achieve this
+
+pumpkin$Crop <- NULL
+pumpkin <- pumpkin[, -23]
+pumpkin <- pumpkin[, -c(21:22)]
+pumpkin <- subset(pumpkin, select = -c(Quality, Condition))
+
 #
-#
+
+attach(pumpkin)
+detach(pumpkin)
+# access columns without 'pumpkin$' (and cancel)
+
+# STANDARDISING FORMATS
+
+# In
+
+pumpkin$Package
+
+# there are multiple unique entries that have different measurements
+
+unique(pumpkin$Package)
+
+# It is crucial to standardise it to kilograms
+
+pumpkin$Package_Numeric <- NA #make a new column
+lb_to_kg <- 0.453592
+inch_to_kg <- 20 #just a guess, inches aren't a measure of weight
+bushel_to_kg <- 10 #another guess
+bins_to_kg <- 5 #more guessing
+each_to_kg <- 1 #you guessed it
+
+for (i in 1:length(pumpkin$Package)) {
+  
+  if (str_detect(pumpkin$Package[i], "lb")) { #check if the value is "lb"
+    temp <- as.numeric(substr(pumpkin$Package[i], start = 1, stop = 2))
+    #extract numeric value
+    pumpkin$Package_Numeric[i] <- temp * lb_to_kg
+    #assign to new column
+  }
+  
+  else if (str_detect(pumpkin$Package[i], "inch")) { 
+    temp <- as.numeric(substr(pumpkin$Package[i], start = 1, stop = 2))
+    pumpkin$Package_Numeric[i] <- temp * inch_to_kg
+  }
+  
+  else if (str_detect(pumpkin$Package[i], "bushel")) {
+    if (str_detect(pumpkin$Package[i], "1 1/9")) {
+      pumpkin$Package_Numeric[i] <- (1+1/9) * bushel_to_kg
+    }
+    else if (str_detect(pumpkin$Package[i], "1/2")) {
+      pumpkin$Package_Numeric[i] <- 1/2 * bushel_to_kg
+    }
+    else {
+      pumpkin$Package_Numeric[i] <- bushel_to_kg
+    }
+  }
+  
+  else if (str_detect(pumpkin$Package[i], "each")) { 
+    pumpkin$Package_Numeric[i] <- each_to_kg
+  }
+  
+  else if (str_detect(pumpkin$Package[i], "bins")) { 
+    pumpkin$Package_Numeric[i] <- bins_to_kg
+  }
+  
+  else {
+    pumpkin$Package_Numeric[i] <- NULL
+  }
+  
+}
+
+# After that, run skim() one more time to check if there are NAs, and then you
+# can safely delete the original pumpkin$Package column
+
+# STANDARDISING DATES
+# Another problem is the date format
+
+class(pumpkin$Date)
+
+# It needs to be a Date class, not a character
+
+pumpkin$Date <- as.Date(pumpkin$Date, format = "%m/%d/%y")
+# we specify the format for the as.Date() function
+
+# ──────────────────────────────────────────────────────────────────────────────
+# OUTLIERS - DETECTING
+# ──────────────────────────────────────────────────────────────────────────────
+
+boxplot(pumpkin$High.Price) # visualises outliers
+
+# you can also try:
+
+ggplot(pumpkin, aes(x = High.Price, y = City.Name)) + 
+  geom_boxplot()
+
+# if you want to see outliers per group
+
+z_scores <- scale(pumpkin$High.Price)  # scale() computes the Z-score
+outliers <- pumpkin$High.Price[abs(z_scores) > 3]  # try 2 or 3
+
+# You can also try
+max(pumpkin$High.Price)
+min(pumpkin$High.Price)
--- README.md
+++ README.md
@@ -1,85 +1,24 @@
 # Exam Template
+Made by **Paweł Gach** with contributions from others
 
 1st semester Machine Learning at AU BSS
 
-Version 0.1
+Version 0.2
 
-2023-11-29
-
-Made by **Paweł Gach** with contributions from others
-
----
-
-SCROLL DOWN FOR TABLE OF CONTENTS
+2023-11-30
 
 ---
 
 This template is meant to be used as a hub of building blocks
 
-When the exam is underway, read the problem, make a plan of the relevant actions your code will need to perform, then go to the respective sections (.R files) and copy the relevant code - do this a few times,😄 and you will have created a code that should be up and running
+When the exam is underway, read the problem, make a plan of the relevant actions your code will need to perform, then go to the respective sections (.R files) and copy the relevant code - do this a few times, and you will have created a code that should be up and running
 
 The dataset used during the exam will be US-pumpkins and the code will refer
 to simply it as "pumpkin":
 
-```r
-pumpkin <- read.csv("US-pumpkins.csv")
-```
-
-with its characteristics being:
-
-```r
-skimr::skim(pumpkin)
-```
-
-```r
-── Data Summary ────────────────────────
-Values
-Name pumpkin
-Number of rows 1757
-Number of columns 26
-_______________________
-Column type frequency:
-character 13
-logical 9
-numeric 4
-________________________
-Group variables None
-── Variable type: character ────────────────────────────────────────────────────────────────────────────────────────────────────────────
-skim_variable n_missing complete_rate min max empty n_unique whitespace
-1 [City.Name](http://city.name/) 0 1 5 13 0 13 0
-2 Type 0 1 0 7 1712 2 0
-3 Package 0 1 4 20 0 15 0
-4 Variety 0 1 0 24 5 11 0
-5 Sub.Variety 0 1 0 10 1461 3 0
-6 Date 0 1 6 8 0 57 0
-7 Origin 0 1 0 14 3 25 0
-8 Origin.District 0 1 0 37 1626 6 0
-9 Item.Size 0 1 0 7 279 8 0
-10 Color 0 1 0 7 616 4 0
-11 Unit.of.Sale 0 1 0 10 1595 5 0
-12 Repack 0 1 1 1 0 2 0
-13 X.1 0 1 0 31 1654 5 0
-── Variable type: logical ──────────────────────────────────────────────────────────────────────────────────────────────────────────────
-skim_variable n_missing complete_rate mean count
-1 Grade 1757 0 NaN ": "
-2 Environment 1757 0 NaN ": "
-3 Quality 1757 0 NaN ": "
-4 Condition 1757 0 NaN ": "
-5 Appearance 1757 0 NaN ": "
-6 Storage 1757 0 NaN ": "
-7 Crop 1757 0 NaN ": "
-8 Trans.Mode 1757 0 NaN ": "
-9 X 1757 0 NaN ": "
-── Variable type: numeric ──────────────────────────────────────────────────────────────────────────────────────────────────────────────
-skim_variable n_missing complete_rate mean sd p0 p25 p50 p75 p100 hist
-1 Low.Price 0 1 125. 83.9 0.24 24 140 180 480 ▆▇▃▁▁
-2 High.Price 0 1 133. 89.5 0.24 24.5 150 200 480 ▇▇▅▁▁
-3 Mostly.Low 103 0.941 128. 86.5 0.24 24.6 147 185 480 ▆▇▅▁▁
-4 Mostly.High 103 0.941 132. 88.4 0.24 26.1 150 200 480 ▆▇▅▁▁
-```
 ## TABLE OF CONTENTS
 
-As this is Version 0.1, this section is subject to change
+As this is not the final version, this section is subject to change
 
 1. Loading and Cleaning
     - Extraction
